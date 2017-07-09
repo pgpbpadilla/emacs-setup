974a9d66610c7f4b66bf6d8478a9133e79c46b13
Pablo Padilla <pgpb.padilla@gmail.com>
2017-07-09 11:28:51 -0700

    chg: dev: Update key bindings for spell checking.

--- init.el
+++ init.el
@@ -119,13 +119,13 @@
 (global-set-key (kbd "<f8>") 'ispell-word)
 (global-set-key (kbd "C-S-<f8>") 'flyspell-mode)
 (global-set-key (kbd "C-M-<f8>") 'flyspell-buffer)
-(global-set-key (kbd "C-<f8>") 'flyspell-check-previous-highlighted-word)
+(global-set-key (kbd "M-<f8>") 'flyspell-check-previous-highlighted-word)
 (defun flyspell-check-next-highlighted-word ()
   "Custom function to spell check next highlighted word"
   (interactive)
   (flyspell-goto-next-error)
   (ispell-word)
   )
-(global-set-key (kbd "M-<f8>") 'flyspell-check-next-highlighted-word)
+(global-set-key (kbd "M-<f9>") 'flyspell-check-next-highlighted-word)
 
 ;;; Init.el ends here
