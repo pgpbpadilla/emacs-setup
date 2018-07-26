35df8cfbcc70d03016c53def5e89aafe3806df93
Pablo Padilla <pgpb.padilla@gmail.com>
2018-07-26 11:53:28 -0700

    chg: usr: Install json-mode on startup

--- init.el
+++ init.el
@@ -10,6 +10,9 @@
   (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
   )
 
+(setq my-package-list '(helm-projectile yaml-mode))
+(mapc #'package-install my-package-list)
+
 ;; Save backups and temp files to a central location to avoid
 ;; certain tools (Grunt-watch) to show annoying ENOENT file because
 ;; I can't figure out a Glob pattern to exclude them from JSLint
@@ -24,11 +27,12 @@
 
 ;;; Shortcut for neotree sidebar
 ;; (require 'neotree)
- (global-set-key [f8] 'neotree-toggle)
+;; (global-set-key [f8] 'neotree-toggle)
 
 
 
 ;; Projectile + Helm
+(package-install 'helm)
 (require 'helm-config)
 (projectile-global-mode)
 (setq projectile-completion-system 'helm)
@@ -52,26 +56,23 @@
  '(electric-indent-mode nil)
  '(global-auto-complete-mode t)
  '(global-auto-highlight-symbol-mode t)
- ;; '(imenu-use-popup-menu nil)
+ '(helm-mode t)
  '(indent-tabs-mode nil)
- '(js2-basic-offset 2)
- '(js2-bounce-indent-p nil)
- '(js2-include-node-externs t)
  '(ns-command-modifier (quote meta))
+ '(package-selected-packages (quote (helm yaml-mode helm-projectile)))
  '(show-paren-mode t)
  '(tab-width 2)
- '(word-wrap nil)
- '(helm-mode t))
+ '(word-wrap nil))
 
 ;;; make transparent
 (set-frame-parameter (selected-frame) 'alpha '(90 90))
 (set-face-attribute 'default nil :height 140)
 
 ;; auto-load js2-mode
-(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+;; (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 
 ;; hide toolbar
-(ns-toggle-toolbar)
+;; (ns-toggle-toolbar)
 ;;; Maximize on startup
 (toggle-frame-maximized)
 
@@ -85,11 +86,11 @@
 
 
 ;; JS stuff 
-(add-hook 'js-mode-hook (lambda () (tern-mode t)))
-(eval-after-load 'tern
-   '(progn
-      (require 'tern-auto-complete)
-      (tern-ac-setup)))
+;;(add-hook 'js-mode-hook (lambda () (tern-mode t)))
+;;(eval-after-load 'tern
+;;   '(progn
+;;      (require 'tern-auto-complete)
+;;      (tern-ac-setup)))
 
 ;; scrolling
 (global-set-key "\M-n"  (lambda () (interactive) (scroll-up   4)) )
@@ -100,32 +101,38 @@
 
 
 ;; AUCTeX
-(setenv "PATH" "/usr/local/bin:/Library/TeX/texbin/:$PATH" t)
-(setq exec-path (append exec-path '("/Library/TeX/texbin")))
-(setq TeX-auto-save t)
-(setq TeX-parse-self t)
-(setq-default TeX-master nil)
+;;(setenv "PATH" "/usr/local/bin:/Library/TeX/texbin/:$PATH" t)
+;;(setq exec-path (append exec-path '("/Library/TeX/texbin")))
+;;(setq TeX-auto-save t)
+;;(setq TeX-parse-self t)
+;;(setq-default TeX-master nil)
+;;
+;;(yas-global-mode 1)
+;;(add-to-list 'yas-snippet-dirs (locate-user-emacs-file "snippets"))
 
-(yas-global-mode 1)
-(add-to-list 'yas-snippet-dirs (locate-user-emacs-file "snippets"))
 
-
-(require 'real-auto-save)
-(add-hook 'prog-mode-hook 'real-auto-save-mode)
-(setq real-auto-save-interval 5) ;; in seconds
+;;(require 'real-auto-save)
+;;(add-hook 'prog-mode-hook 'real-auto-save-mode)
+;;(setq real-auto-save-interval 5) ;; in seconds
 
 ;; Flyspell
 ;; easy spell check
-(global-set-key (kbd "<f8>") 'ispell-word)
-(global-set-key (kbd "C-S-<f8>") 'flyspell-mode)
-(global-set-key (kbd "C-M-<f8>") 'flyspell-buffer)
-(global-set-key (kbd "M-<f8>") 'flyspell-check-previous-highlighted-word)
-(defun flyspell-check-next-highlighted-word ()
-  "Custom function to spell check next highlighted word"
-  (interactive)
-  (flyspell-goto-next-error)
-  (ispell-word)
-  )
-(global-set-key (kbd "M-<f9>") 'flyspell-check-next-highlighted-word)
+;;(global-set-key (kbd "<f8>") 'ispell-word)
+;;(global-set-key (kbd "C-S-<f8>") 'flyspell-mode)
+;;(global-set-key (kbd "C-M-<f8>") 'flyspell-buffer)
+;;(global-set-key (kbd "M-<f8>") 'flyspell-check-previous-highlighted-word)
+;;(defun flyspell-check-next-highlighted-word ()
+;;  "Custom function to spell check next highlighted word"
+;;  (interactive)
+;;  (flyspell-goto-next-error)
+;;  (ispell-word)
+;;  )
+;;(global-set-key (kbd "M-<f9>") 'flyspell-check-next-highlighted-word)
 
 ;;; Init.el ends here
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
